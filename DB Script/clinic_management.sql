39bb3f6ae4eaa22277a23b2e235513a9392cee52
Santo Brighton <santo.brighton2001@gmail.com>
2022-08-29 04:16:21 +0530

    Final Commit

--- DB Script/clinic_management.sql
+++ DB Script/clinic_management.sql
@@ -1,32 +1,47 @@
+--Creating the database for Clinical Management System
 create database Clinic
 
 use Clinic
 
+--Creating users table (Admin)
 create table users (username varchar(10) unique constraint check_username check(username not like '%[^a-zA-Z0-9]%'),
 firstname varchar(20), lastname varchar(20), password varchar(25) constraint check_password check(password like '%@%'))
 
+--Inserting the user values
 insert into users values ('santo19', 'Santo', 'Brighton', 'Santo@19')
 insert into users values ('deepak11', 'Deepak', 'Kumar', 'Deepak@11'),('atul07', 'Atul', 'Lakkapragada', 'Atul@07'),('basid29', 'Mohammed', 'Basid', 'Basid@29')
 
+--Displaying all the users
+select * from users
+
+--Creating doctors able
 create table doctors (doctor_id int primary key, firstname varchar(20) constraint check_firstname check(firstname not like '%[^a-zA-Z0-9]%'), 
 lastname varchar(20) constraint check_lastname check(lastname not like '%[^a-zA-Z0-9]%'), 
 sex varchar(7), specialization varchar(50), visiting_from time, visiting_to time)
 
+--Inserting the doctor values
 insert into doctors values (101, 'Kiruba', 'Karan', 'M', 'Orthopedics', '14:00', '17:00'),
 (102, 'Kevin', 'Anderson', 'M', 'Internal Medicine', '18:00', '21:00'),
 (103, 'Sharon', 'Angel', 'F', 'Pediatrics', '09:00', '12:00'),
 (104, 'Seetha', 'Mahalakshmi', 'F', 'General', '10:00', '12:00'),
 (105, 'Dinesh', 'Moorthy', 'M', 'Opthamology', '16:00', '18:00')
 
+--Displaying all the doctors
+select * from doctors
 
+--Creating patients table
 create table patients (patient_id int identity(2000, 1) primary key, firstname varchar(20) constraint check_firstname_patient check(firstname not like '%[^a-zA-Z0-9]%'),
 lastname varchar(20) constraint check_lastname_patient check(lastname not like '%[^a-zA-Z0-9]%'), sex varchar(7),
 age int constraint check_age_patient check(age between 0 and 120), dob datetime)
 
+--Displaying all the patients
+select * from patients
 
+--Creating appointments table
 create table appointments (apt_id int identity(500,1) primary key, doctor_id int foreign key references doctors(doctor_id), visiting_date date,
 timeslot varchar(30), apt_status varchar(30), patient_id int foreign key references patients(patient_id))
 
+--Inserting the appointment values
 insert into appointments values (101, '2022-08-26', '14:00 - 15:00', 'Available', null),
 (101, '2022-08-26',  '15:00 - 16:00', 'Available', null),
 (101, '2022-08-26', '16:00 - 17:00', 'Available', null),
@@ -56,7 +71,7 @@ insert into appointments values (101, '2022-08-27', '14:00 - 15:00', 'Available'
 (105, '2022-08-27', '17:00 - 18:00', 'Available', null)
 
 insert into appointments values (101, '2022-08-28', '14:00 - 15:00', 'Available', null),
-(101,'2022-08-28', '15:00 - 16:00', 'Available', null),
+(101, '2022-08-28', '15:00 - 16:00', 'Available', null),
 (101, '2022-08-28', '16:00 - 17:00', 'Available', null),
 (102, '2022-08-28', '18:00 - 19:00', 'Available', null),
 (102, '2022-08-28', '19:00 - 20:00', 'Available', null),
@@ -69,16 +84,52 @@ insert into appointments values (101, '2022-08-28', '14:00 - 15:00', 'Available'
 (105, '2022-08-28', '16:00 - 17:00', 'Available', null),
 (105, '2022-08-28', '17:00 - 18:00', 'Available', null)
 
-drop table users
-drop table doctors
-drop table patients
-drop table appointments
+insert into appointments values (101, '2022-08-29', '14:00 - 15:00', 'Available', null),
+(101, '2022-08-29', '15:00 - 16:00', 'Available', null),
+(101, '2022-08-29', '16:00 - 17:00', 'Available', null),
+(102, '2022-08-29', '18:00 - 19:00', 'Available', null),
+(102, '2022-08-29', '19:00 - 20:00', 'Available', null),
+(102, '2022-08-29', '20:00 - 21:00', 'Available', null),
+(103, '2022-08-29', '09:00 - 10:00', 'Available', null),
+(103, '2022-08-29', '10:00 - 11:00', 'Available', null),
+(103, '2022-08-29', '11:00 - 12:00', 'Available', null),
+(104, '2022-08-29', '10:00 - 11:00', 'Available', null),
+(104, '2022-08-29', '11:00 - 12:00', 'Available', null),
+(105, '2022-08-29', '16:00 - 17:00', 'Available', null),
+(105, '2022-08-29', '17:00 - 18:00', 'Available', null)
 
-select * from users
-select * from doctors
-select * from patients
+insert into appointments values (101, '2022-08-30', '14:00 - 15:00', 'Available', null),
+(101, '2022-08-30', '15:00 - 16:00', 'Available', null),
+(101, '2022-08-30', '16:00 - 17:00', 'Available', null),
+(102, '2022-08-30', '18:00 - 19:00', 'Available', null),
+(102, '2022-08-30', '19:00 - 20:00', 'Available', null),
+(102, '2022-08-30', '20:00 - 21:00', 'Available', null),
+(103, '2022-08-30', '09:00 - 10:00', 'Available', null),
+(103, '2022-08-30', '10:00 - 11:00', 'Available', null),
+(103, '2022-08-30', '11:00 - 12:00', 'Available', null),
+(104, '2022-08-30', '10:00 - 11:00', 'Available', null),
+(104, '2022-08-30', '11:00 - 12:00', 'Available', null),
+(105, '2022-08-30', '16:00 - 17:00', 'Available', null),
+(105, '2022-08-30', '17:00 - 18:00', 'Available', null)
+
+insert into appointments values (101, '2022-08-31', '14:00 - 15:00', 'Available', null),
+(101, '2022-08-31', '15:00 - 16:00', 'Available', null),
+(101, '2022-08-31', '16:00 - 17:00', 'Available', null),
+(102, '2022-08-31', '18:00 - 19:00', 'Available', null),
+(102, '2022-08-31', '19:00 - 20:00', 'Available', null),
+(102, '2022-08-31', '20:00 - 21:00', 'Available', null),
+(103, '2022-08-31', '09:00 - 10:00', 'Available', null),
+(103, '2022-08-31', '10:00 - 11:00', 'Available', null),
+(103, '2022-08-31', '11:00 - 12:00', 'Available', null),
+(104, '2022-08-31', '10:00 - 11:00', 'Available', null),
+(104, '2022-08-31', '11:00 - 12:00', 'Available', null),
+(105, '2022-08-31', '16:00 - 17:00', 'Available', null),
+(105, '2022-08-31', '17:00 - 18:00', 'Available', null)
+
+--Displaying all the appointments
 select * from appointments
 
+--Sample examples for stored procedure
 --Stored Procedures
 create procedure spselectUserLogin(@username varchar(10), @password varchar(25))  as 
 select * from users where username = @username and password = @password
@@ -96,6 +147,3 @@ create procedure spinsertAddPatients (@firstname varchar(20), @lastname varchar(
 as insert into patients (firstname, lastname, sex, age, dob) values (@firstname, @lastname, @sex, @age, @dob)
 
 execute spinsertAddPatients
-
-create procedure spselectAddPatients (@firstname varchar(20), @lastname varchar(20), @sex varchar(7), @age int, @dob datetime)
-as select patient_id from patients where firstname=@firstname and lastname=@lastname and sex=@sex and age=@age and dob=@dob
--- src/ClinicLibClient/ClinicLibClient/Program.cs
+++ src/ClinicLibClient/ClinicLibClient/Program.cs
@@ -2,15 +2,16 @@
 using System.Data.SqlClient;
 using ClinicLib;
 namespace ClinicLibClient
+//Front-end application for the Eurofins Clinical Management System
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("************************************");
-            Console.WriteLine("********* Eurofins Clinic **********");
-            Console.WriteLine("************************************");
-            Console.WriteLine("~~ Welcome ~~");
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("********* EUROFINS CLINICAL MANAGEMENT SYSTEM **********");
+            Console.WriteLine("********************************************************");
+            Console.WriteLine("~~~~~ Welcome ~~~~~");
             while (true)
             {
 
@@ -19,9 +20,9 @@ namespace ClinicLibClient
                     
                     Console.WriteLine("-----------------------------------");
                     Console.WriteLine("*** Login ***");
-                    Console.WriteLine("Enter the username:");
+                    Console.Write("Enter the username: ");
                     string username = Console.ReadLine();
-                    Console.WriteLine("Enter the password:");
+                    Console.Write("Enter the password: ");
                     string password = Console.ReadLine();
                     Login login = new Login();
                     IHome home = new Home();
@@ -29,24 +30,27 @@ namespace ClinicLibClient
                     ICancelSchedule cs = new CancelSchedule();
                     login.UserLogin(username, password);
                     Console.WriteLine("-----------------------------------");
-                    Console.WriteLine("User login successful!");
+                    Console.WriteLine("Login Successful!");
                     Console.WriteLine("-----------------------------------");
                     Console.WriteLine("Home Page");
                     Console.WriteLine("-----------------------------------");
                     while (true)
                     {
                         Console.WriteLine("-----------------------------------");
-                        Console.WriteLine("-----------------------------------");
-                        Console.WriteLine("1.View all the Doctors in the Clinic");
-                        Console.WriteLine("2.Add new Patient");
-                        Console.WriteLine("3.Schedule your Appointment");
-                        Console.WriteLine("4.Cancel your Appointment");
-                        Console.WriteLine("5.Logout");
-                        Console.WriteLine("-----------------------------------");
-                        Console.WriteLine("Enter the option");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.WriteLine("Click option '1' to View all the Doctors in the Clinic");
+                        Console.WriteLine("Click option '2' to Add new Patient");
+                        Console.WriteLine("Click option '3' to View all the Patients in the Clinic");
+                        Console.WriteLine("Click option '4' to Schedule your Appointment");
+                        Console.WriteLine("Click option '5' to Cancel your Appointment");
+                        Console.WriteLine("Click option '6' to Logout");
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        Console.Write("Enter the option of your choice: ");
                         int option = Convert.ToInt32(Console.ReadLine());
-                        if (option == 5)
+                        if (option == 6)
                         {
+                            Console.WriteLine("You are successfully logged out!");
+                            Console.WriteLine("Thank You for trusting us!!!");
                             break;
                         }
                         switch (option)
@@ -54,6 +58,7 @@ namespace ClinicLibClient
                             case 1:
                                 {
                                     List<Doctors> doc = home.viewAllDoctors();
+                                    Console.WriteLine("\t\t---Doctor Details---");
                                     foreach (Doctors d in doc)
                                     {
                                         Console.WriteLine("-------------------------------------");
@@ -71,15 +76,16 @@ namespace ClinicLibClient
                                     try
                                     {
                                         Console.WriteLine("-------------------------------------");
-                                        Console.WriteLine("Enter the first name:");
+                                        Console.WriteLine("\t\t---Adding Patient Details---");
+                                        Console.Write("Enter the first name: ");
                                         string first_name = Console.ReadLine();
-                                        Console.WriteLine("Enter the last name:");
+                                        Console.Write ("Enter the last name: ");
                                         string last_name = Console.ReadLine();
-                                        Console.WriteLine("Enter the sex:");
+                                        Console.Write ("Enter the sex: ");
                                         string sex = Console.ReadLine();
-                                        Console.WriteLine("Enter the age:");
+                                        Console.Write ("Enter the age: ");
                                         int age = Convert.ToInt32(Console.ReadLine());
-                                        Console.WriteLine("Enter the dob:");
+                                        Console.Write("Enter the date of birth: ");
                                         string dob = Console.ReadLine();
                                         home.validatePatients(first_name, last_name, sex, age, dob);
                                         DateTime dateofbirth = Convert.ToDateTime(dob);
@@ -104,22 +110,38 @@ namespace ClinicLibClient
                                     Console.WriteLine("-------------------------------------");
                                     break;
                                 }
-
                             case 3:
+                                {
+                                    List<Patients> pat = home.viewAllPatients();
+                                    Console.WriteLine("\t\t\t---Patient Details---");
+
+                                    foreach (Patients ps in pat)
+                                    {
+                                        Console.WriteLine($"Patient ID : {ps.patientID} \nFirstName : {ps.first_name}\n" +
+                                            $"LastName : {ps.last_name} \nSex : {ps.sex} \nAge : {ps.age}" +
+                                            $"\nDate Of Birth : {ps.dob}\n -------------------------------");
+                                    }
+                                    break;
+                                }
+
+                            case 4:
                                 {
                                     try
                                     {
                                         Console.WriteLine("-------------------------------------");
-                                        Console.WriteLine("Enter the patient id");
+                                        Console.Write("Enter the Patient ID: ");
                                         int pid = Convert.ToInt32(Console.ReadLine());
                                         Console.WriteLine("-------------------------------------");
                                         Console.WriteLine("Available specializations : General, Orthopedics, Internal Medicine, Pediatrics, Opthalmology");
                                         Console.WriteLine("-------------------------------------");
-                                        Console.WriteLine("Enter the specialization");
+                                        Console.Write("Enter the specialization: ");
                                         string spec = Console.ReadLine();
 
                                         sa.ValidateScheduleAppointment(pid, spec);
                                         List<Doctors> doc = sa.displayDoctorsBasedOnSpecialization(spec);
+                                        List<int> valdocid = new List<int>();
+                                        Console.WriteLine("\t\t---Doctor Specialization Details---");
+
                                         foreach (Doctors d in doc)
                                         {
                                             Console.WriteLine("-------------------------------------");
@@ -129,14 +151,17 @@ namespace ClinicLibClient
 
                                             Console.WriteLine("-------------------------------------");
                                         }
-                                        Console.WriteLine("The available dates for booking are  : [26/08/2022,27/08/2022,28/08/2022]");
-                                        Console.WriteLine("Enter the Date of Appointment");
+                                        Console.WriteLine("The available dates for booking are  : [26/08/2022, 27/08/2022, 28/08/2022, 29/08/2022, 30/08/2022, 31/08/2022]");
+                                        Console.Write("Enter the Date of Appointment: ");
                                         string dateofapp = Console.ReadLine();
+                                        sa.ValidateDateLimit(dateofapp);
                                         sa.ValidateIndianFormatDate(dateofapp);
-                                        Console.WriteLine("Enter the Doctor id");
+                                        Console.Write("Enter the Doctor ID: ");
                                         int docid = Convert.ToInt32(Console.ReadLine());
+                                        sa.ValidateDoctorID(docid, valdocid);
                                         DateTime dateofappointment = DateTime.Parse(dateofapp);
                                         List<Appointments> app = sa.getAllSlotsForDoctor(docid, dateofappointment);
+                                        List<int> apid = new List<int>();
                                         Console.WriteLine("-------------------------------------");
                                         Console.WriteLine("The available slots are");
                                         Console.WriteLine("-------------------------------------");
@@ -150,8 +175,9 @@ namespace ClinicLibClient
                                             Console.WriteLine("-------------------------------------");
                                         }
                                         Console.WriteLine("-----------------------------------");
-                                        Console.WriteLine("Enter the Appointment ID");
+                                        Console.Write("Enter the Appointment ID: ");
                                         int appointmentId = Convert.ToInt32(Console.ReadLine());
+                                        sa.ValidateAppointmentID(apid, appointmentId);
                                         sa.bookAppointment(appointmentId, pid);
                                         Console.WriteLine("Apoointment successfully booked for the Patient ID " + pid);
                                         Console.WriteLine("Your Appointment ID is " + appointmentId);
@@ -168,17 +194,19 @@ namespace ClinicLibClient
                                     break;
                                 }
 
-                            case 4:
+                            case 5:
                                 {
                                     try
                                     {
-                                        Console.WriteLine("Enter the Patient ID");
+                                        Console.WriteLine("\t\t----Cancellation----");
+                                        Console.Write("Enter the Patient ID: ");
                                         int pid = Convert.ToInt32(Console.ReadLine());
 
                                         cs.ValidatePatientID(pid);
-                                        Console.WriteLine("The available dates for cancellation  are : [26/08/2022,27/08/2022,28/08/2022]");
-                                        Console.WriteLine("Enter the Cancel date");
+                                        Console.WriteLine("The available dates for cancellation  are : [26/08/2022, 27/08/2022, 28/08/2022, 29/08/2022, 30/08/2022, 31/08/2022]");
+                                        Console.Write("Enter the Cancel date: ");
                                         string date = Console.ReadLine();
+                                        sa.ValidateDateLimit(date);
                                         cs.ValidateIndianFormatDate(date);
 
                                         DateTime dt = Convert.ToDateTime(date);
@@ -203,7 +231,7 @@ namespace ClinicLibClient
                                             }
                                             Console.WriteLine("-----------------------------------");
 
-                                            Console.WriteLine("Enter the Appointment ID to cancel");
+                                            Console.Write("Enter the Appointment ID to cancel: ");
                                             int appid = Convert.ToInt32(Console.ReadLine());
                                             cs.cancelAppointment(appid, pid);
                                             Console.WriteLine("Appointment of ID " + appid + " is cancelled successfully");
